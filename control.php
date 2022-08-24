ab88072127b2bf45941a75e188781e9dcfbd4ac7
Albandry209 <108113846+Albandry209@users.noreply.github.com>
2022-08-24 16:46:09 +0300

    Add files via upload

---
+++ control.php
@@ -0,0 +1,27 @@
+<?php
+
+if(isset($_POST['forward']))
+{
+print "f";
+}
+if(isset($_POST['left'])
+ {
+ print "l";
+ }
+
+ if(isset($_POST['backward'])
+ {
+ print "b";
+ }
+
+ else if(isset($_POST['right']) )
+ {
+ print "r";
+ }
+ else if(isset($_POST['stop']) )
+ {
+     echo "s";
+ }
+  // Create connection
+  exit;
+  ?>
